201868591d070fb5282023902f0edfe7538ad0d2
pvvishwesh-lang <pv.vishwesh@gmail.com>
2026-05-16 17:20:35 -0400

    Lazy loading to fetch only changed movies from TMDB and update Qdrant with new embeddings.

--- backend/embedder.py
+++ backend/embedder.py
@@ -1,7 +1,12 @@
 from sentence_transformers import SentenceTransformer
 
-
-model=SentenceTransformer('all-MiniLM-L6-v2')
+model = None
+def get_model():
+    global model
+    if model is None:
+        model = SentenceTransformer('all-MiniLM-L6-v2')
+    return model
 
 def generate_embedding(input_text):
+    model = get_model()
     return model.encode(input_text, show_progress_bar=False)
--- backend/vector_store.py
+++ backend/vector_store.py
@@ -5,10 +5,16 @@ import os
 CLUSTER_ENDPOINT=os.environ.get('CLUSTER_ENDPOINT')
 QDRANT_API_KEY=os.environ.get('QDRANT_API_KEY')
 
+client = None
 
-client=QdrantClient(url=CLUSTER_ENDPOINT,api_key=QDRANT_API_KEY)
+def get_client():
+    global client
+    if client is None:
+        client=QdrantClient(url=CLUSTER_ENDPOINT,api_key=QDRANT_API_KEY)
+    return client
 
 def search_movies(embedding,limit=10):
+    client=get_client()
     results=client.query_points(collection_name='movies',query=embedding,limit=limit).points
     data=[{"title": r.payload["title"],"overview": r.payload["overview"],"rating": r.payload["rating"],"poster_path": r.payload["poster_path"],"genres": r.payload["genres"],"release_date": r.payload["release_date"],"score": r.score} for r in results]
     return data
