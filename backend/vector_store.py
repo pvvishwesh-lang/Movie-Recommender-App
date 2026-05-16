e390b2ba7cc933c176430777cd8d26e737d78d13
pvvishwesh-lang <pv.vishwesh@gmail.com>
2026-05-16 16:32:32 -0400

    Changes

--- backend/vector_store.py
+++ backend/vector_store.py
@@ -2,7 +2,9 @@ from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, QueryResponse
 import os
 
-client=QdrantClient(url=os.environ.get('CLUSTER_ENDPOINT'),api_key=os.environ.get('QDRANT_API_KEY'))
+CLUSTER_ENDPOINT=os.environ.get('CLUSTER_ENDPOINT')
+QDRANT_API_KEY=os.environ.get('QDRANT_API_KEY')
+client=QdrantClient(url=CLUSTER_ENDPOINT,api_key=QDRANT_API_KEY)
 
 def search_movies(embedding,limit=10):
     results=client.query_points(collection_name='movies',query=embedding,limit=limit).points
