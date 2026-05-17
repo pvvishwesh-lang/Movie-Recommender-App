e40a461ea5bb8cf5eb56f31a45cbb3f164008b40
Vishwesh P V <pv.vishwesh@gmail.com>
2026-05-17 14:56:50 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
-
 class Recommendations(BaseModel):
     title:str
     overview:str
