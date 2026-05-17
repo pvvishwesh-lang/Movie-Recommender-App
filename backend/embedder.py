0fbebbb68dd327a0186b37b7128bb303936839cd
pvvishwesh-lang <pv.vishwesh@gmail.com>
2026-05-16 23:08:51 -0400

    URL CHANGE

--- backend/embedder.py
+++ backend/embedder.py
@@ -5,7 +5,7 @@ HF_TOKEN = os.environ.get("HF_TOKEN")
 
 
 def generate_embedding(input_text):
-    response = requests.post("https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2", headers={"Authorization": f"Bearer {HF_TOKEN}"},json={"inputs": input_text})
+    response = requests.post("https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2", headers={"Authorization": f"Bearer {HF_TOKEN}"},json={"inputs": input_text})
     print(f"HF API status: {response.status_code}")
     print(f"HF API response: {response.text[:200]}")
     result=response.json()
