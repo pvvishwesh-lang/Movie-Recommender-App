ca505ce7525037adfb81e79c5a495c2fd92a2210
Vishwesh P V <pv.vishwesh@gmail.com>
2026-05-17 19:28:39 -0400

    Remove unnecessary blank line in embedder.py

--- backend/embedder.py
+++ backend/embedder.py
@@ -4,7 +4,6 @@ import os
 
 HF_TOKEN = os.environ.get("HF_TOKEN")
 
-
 def generate_embedding(input_text):
     response = requests.post("https://router.huggingface.co/hf-inference/models/sentence-transformers/all-MiniLM-L6-v2/pipeline/feature-extraction", headers={"Authorization": f"Bearer {HF_TOKEN}"},json={"inputs": input_text})
     print(f"HF API status: {response.status_code}")
